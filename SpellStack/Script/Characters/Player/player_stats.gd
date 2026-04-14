00d3af7b0d8658325c25b7c3def397dd1715fad5
Alexlegamer427 <alexisgrenier12@outlook.com>
2026-04-14 18:23:22 -0400

    add working hp bar with textures

--- SpellStack/Script/Characters/Player/player_stats.gd
+++ SpellStack/Script/Characters/Player/player_stats.gd
@@ -13,3 +13,7 @@ func _init():
 @export var deck_size: int = 4
 @export var energy: int = 2;
 @export var luck: int = 0
+
+func take_damage(amount):
+	current_hp -= clamp(amount,0,max_hp)
+	Events.emit_signal("player_health_changed", current_hp, max_hp)
