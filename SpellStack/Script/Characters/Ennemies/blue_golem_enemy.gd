69ef27726c3127df12774510541cc22796acb56a
dragiole <115568131+dragiole@users.noreply.github.com>
2026-05-01 13:43:00 -0400

    Pre-merge commit on SoundSystem

--- SpellStack/Script/Characters/Ennemies/blue_golem_enemy.gd
+++ SpellStack/Script/Characters/Ennemies/blue_golem_enemy.gd
@@ -3,7 +3,6 @@ extends Ennemi
 # class parent qui fera juste le tour des actions principales
 #chaque ennemi va heriter de Ennemi
 class_name BlueGolem
-@onready var SoundController: Node = $"../SoundManager"
 
 func _ready() -> void:
 	stats_path = "res://Resources/Stats/BlueGolem.tres"
